6cd13c9cab38be9a97b7365726fdd0dd894a21ff
Annamalai Annamalai <annamalai1.a@gmail.com>
2019-04-23 14:43:40 -0400

    Initial iOS Project Creation, no edits have been made from the defaults of Xcode

---
+++ StockAlert/StockAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockAlert
+//
+//  Created by Annamalai Annamalai on 4/23/19.
+//  Copyright © 2019 Annamalai Annamalai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
